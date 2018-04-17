0db54356a12744c174bcdbef53bd753cd8ab60ee
Rodrigo Sousa <rodrigo.sousa@ua.pt>
2018-04-17 17:48:50 +0100

    Landing page, stylesheets e algumas imagens. One small step for man, etc etc...

---
+++ landing.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Cubo</title>
+
+</head>
+
+<body>
+
+    <div id="conteudo">
+
+        <img src="img/cubo.png" class="cuboinicial" >
+
+        <h1>desafia-te</h1>
+
+        <button id="login">Login</button>
+
+        <button id="registar">Registar</button>
+
+    </div>
+
+
+</body>
+
+</html>
