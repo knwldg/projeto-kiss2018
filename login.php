2bc08084fc1e5a340e5c79887ed7b77f5219b254
Marialmeida <marii.corte.real@gmail.com>
2018-05-16 17:36:52 +0100

    Pasta de imagens com os icons; Login

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Cubo</title>
+</head>
+
+<body>
+
+<div id="laranja">
+    <img src="img/marca/logo.png" class="cubologin" >
+
+    <div id="caixa">
+
+        <h2>Log In</h2>
+
+        <form id="log">
+
+            <img src="img/icon/user.png" class="loginimg">
+            <!-- Parte do formulário para utilizador-->
+            <input type="text" name="username" placeholder="Username" class="formlogin">
+            </br>
+            <img src="img/icon/pass_icon.png" class="loginimg">
+            <!-- Parte do formulário para pass-->
+            <input type="password" name="pass" placeholder="Password" class="formlogin">
+            </br>
+            <input type="submit" name="submitlogin" class="submit">
+        </form>
+
+    </div>
+
+
+</div>
+
+</body>
+</html>
