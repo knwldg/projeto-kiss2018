7c3db12b408d73a1f678f55e95f63b99d87f34a6
eljmmarinho <jm_junior@live.com.pt>
2018-05-21 16:14:37 +0100

    premios.php

---
+++ premios.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Prémios</title>
+</head>
+
+<body>
+<div id="premios">
+    <h2>Nome do Prémio</h2>
+    <div class="cartas_premios">
+
+    </div>
+    <img class="cubo_premios" src="img/marca/logo.png">
+</div>
+<img class="chest_down" src="">
+
+</body>
