6d65f54f1e9f11b823646891abf67585d8116762
eljmmarinho <jm_junior@live.com.pt>
2018-05-21 17:25:30 +0100

    desafios_update

--- desafios.php
+++ desafios.php
@@ -11,31 +11,17 @@
 </head>
 
 <body>
-<div id="desafios">
-    <!-- /.row -->
-    <div id="desafios_title">
-        <h1>Baús</h1>
-    </div>
-    <div id="table_desafios">
-        <table>
-            <tr>
-                <th>Nome</th>
-                <th>Descrição</th>
-                <th>Imagem</th>
-                <th>Chaves Atribuidas</th>
-            </tr>
-        </table>
-    </div>
+<div id="atividades">
+<div class="conteudos_desafios">
+    <img src="" id="logo_desafios">
+    <h2>Nome do Desafio</h2>
+    <h3>Dessafio:</h3>
+    <p>Descrição do Desafio</p>
+    <img src="" class="chaves_desafios">
+    <img src="" class="relógio" >
+    <p>TEMPO</p>
 </div>
-<div>
-    <button class="apagar_desafio">
-        Apagar Desafio
-    </button>
-    <button class="adicionar_desafio">
-        Adicionar Desafio
-    </button>
-    <button class="editar_desafio">
-        Editar Desafio
-    </button>
+    <img class="chest_down" src="">
 </div>
+
 </body>
