b166b1ebfea88b84983f59107a89a72a2a434e5c
EleonorSilva <eleonorsilva123@gmail.com>
2018-05-22 19:53:48 +0100

    update menu and homepage

---
+++ components/public/menu.php
@@ -0,0 +1,49 @@
+<style>
+    <?php include "../../css/style.css"; ?>
+</style>
+
+<div id="back">
+
+    <div id="main-menu">
+
+        <div id="profile-menu">
+        <?php include "profile_details.php"; ?>
+        </div>
+
+        <div id="menu-pages" style="text-align: center"><!-- responsive mode: width 15% -->
+
+            <ul style="list-style-type:none; padding-left: 0.3em">
+                <li style="border-top: 1px solid black; border-bottom: 1px solid black"><img src="">PÁGINA PRINCIPAL<span></span></li><!--icon e seta-->
+                <li>
+                    <ul id="main-pages" style="list-style-type:none; padding-left: 0.3em; text-align: left; display: block"> <!-- main pages -->
+
+                        <li style="border-bottom: 1px solid black">DESAFIO DO DIA</li>
+                        <li style="border-bottom: 1px solid black">BAÚS</li>
+                        <li style="border-bottom: 1px solid black">DESAFIOS</li>
+                        <li style="border-bottom: 1px solid black">COLEÇÕES</li>
+                        <li style="border-bottom: 1px solid black">PRÊMIOS</li>
+
+                    </ul>
+                </li>
+                <li><img src="">NOVIDADES<span></span></li><!--icon e seta-->
+                <li>
+                    <ul id="notification-pages" style="list-style-type:none; padding-left: 0.3em; text-align: left; display: block"> <!-- notification pages -->
+
+                        <li>Tea</li>
+                        <li>Milk</li>
+                    </ul>
+                </li>
+
+
+                <li><img src="">AJUDA</li><!--icon-->
+
+                <li><img src="">SAIR</li><!--icon-->
+
+
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>
