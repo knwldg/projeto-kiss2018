661d68488d17992ce0f3020c90566756b3268724
Marialmeida <marii.corte.real@gmail.com>
2018-05-23 16:26:29 +0100

    Paginas: Coleçoes; info e registo

---
+++ info.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Cubo</title>
+</head>
+
+<body>
+
+<div id="info">
+    <h1>
+        O que é o Cubo?
+    </h1>
+    <p class="info_cubo">
+O Cubo é uma web-app que pretende tornar atividades e ações civicas num jogo !
+        Uma forma dinamica de ganhares prémios, fazendo boas ações!
+    <p class="info_strong">
+        Explora o Jogo!
+    </p>
+    </p>
+    <h1>
+        Como Jogar?
+    </h1>
+    <p class="info_cubo">
+Completa Desafios para obteres chaves !
+        Com as chaves podes abrir baús, que contêm cromos que podes colecionar e trocar por prémios!
+        Os baús podem ser comuns (baús azuis) e dão te x cromos, mas há sempre a possibilidade de ser um baú raro (baú dourado)que te dá mais e melhores cromos!
+        Os cromos também têm grau de raridade, sendo que podes ficar com alguns repetidos, que te permite evoluir o cromo dourado (mais raro)
+        Para reclamares o teu prémio, vai á pagina dos prémios e segue o que a descrição do prémio te diz!
+        <p class="info_strong">
+        Diverte-te
+    </p>
+    <h1>
+        Desenvolvido por:
+    </h1>
+    <p class="info_cubo">
+    O jogo Cubo foi desenvolvido no ambito da disciplina de Projeto da licenciatura de Novas Tecnologias da Comunicação,
+        pelos alunos : Eleonor Silva; José Marinho; Maria Almeida e Rodrigo Sousa, orientados pelo professor Óscar Mealha.
+        Está inserido tambem no projeto KISS (knowledge interface school-society), e em co-desgin com membros do Agrupamento de Escolas José Estevão!
+        São tambem essenciais para o desenvolvimento deste projeto, os alunos que puderam participar nos grupos focais, e deram uma ajuda especial aos desenvolvedores da web-app!
+
+    </p>
+    <h1>
+        Agradecimentos
+    </h1>
+    <p class="info_cubo">
+Agradecemos a todos envolvidos neste projeto, mas em especial a:
+        Professor Óscar Mealha;
+        Professor Fernando Delgado;
+        Professora Carla Carvalho;
+        Professora Anabela Pais;
+    </p>
+</div>
+
+</body>
+</html>
