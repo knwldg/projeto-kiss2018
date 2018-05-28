9d2f2d1a6739efdcfd179cb7b2eb28218fdc8611
Marialmeida <marii.corte.real@gmail.com>
2018-05-28 18:00:40 +0100

    Imagens de bau e coleção da equipa; html dos modais de abertura de bau e abrir cartas

---
+++ modal/bau.php
@@ -0,0 +1,41 @@
+<div id="modal" class="modalbau">
+
+
+    <!--  div para escolher bau/matriz 3X3 -->
+<div id="matrizbaus">
+
+    <img src="../img/bau/baufechado.png" class="baumatriz" id="bau1">
+    <img src="../img/bau/baufechado.png" class="baumatriz" id="bau2">
+    <img src="../img/bau/baufechado.png" class="baumatriz" id="bau3">
+    <img src="../img/bau/baufechado.png" class="baumatriz" id="bau4">
+    <img src="../img/bau/baufechado.png" class="baumatriz" id="bau5">
+    <img src="../img/bau/baufechado.png" class="baumatriz" id="bau6">
+
+
+</div>
+
+    <!--  animação para abrir bau -->
+
+<div id="abrirbau">
+
+    <img src="../img/bau/baufechado.png" class="abrir" >
+
+</div>
+
+    <!--  display das cartas obtidas com o bau -->
+
+<div id="mostrarcartas">
+
+    <img src="" class="cartas">
+
+</div>
+
+    <!--  Bau ja aberto  -->
+
+<div id="fimbau">
+
+    <img src="../img/bau/bauaberto.png" class="fechar">
+
+</div>
+
+</div>
